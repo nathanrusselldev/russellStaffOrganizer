0ab1c0957f6cb8e5a526bb3530a6bc06f156da9c
nathanrusselldev <nathanrussell.it@gmail.com>
2022-02-23 21:35:55 -0500

    completed all main functionality and generation of page

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const Employee = require('./lib/employee.js')
 const Manager = require('./lib/manager.js')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
-const { data } = require('browserslist')
-
 const teamArray = []
 
 
@@ -67,10 +65,16 @@ function createTeam() {
                         addIntern()
                         break
                     case 'No, my team is finished.':
-                        console.log("You have created your team!")
-                        console.log(teamArray)
+                        pushTeam()
+                        console.log(`
+                        ********
+                        You have created your team! Please check the dist folder for your new website.
+                        ********
+                        `);      
+                        return teamArray
+                        
+                        
                 }
-
             })
 }
 
@@ -137,6 +141,77 @@ function addIntern() {
 
 }
 
-createManager()
 
-console.log("this is a test update")
+
+
+
+function pushTeam() {
+
+const siteArray = []
+
+const htmlHead = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>My Team</title>
+
+</head>
+<body>
+<h1 class="col-12 bg-danger" style="height:100px; color: blue; border-bottom: 2px black; border-style: outset; "><span class="d-flex justify-content-center align-content-center">My Team </span></h1>
+`
+siteArray.push(htmlHead)
+
+teamArray.forEach((element) => {
+    let card = `
+    <div class="card" style="width: 18rem;">
+        <div>
+            <div class="card-top bg-primary" >
+            <h2 class="card-title mb-3" style="border-bottom: 2px black;">${element.name} </h2>
+            <h3 class="card-title mb-3">${element.role}</h3>
+            </div>
+            <a href="mailto:${element.email}" class="card-link">E-mail</a>
+            <p>Employee Id: ${element.id}</p>
+       
+    `
+    if (`${element.role}` === "Manager") {
+        card +=`
+        <p>Employee Id: ${element.officeNumber}</p>
+        `
+    }
+    if (`${element.role}` === "Engineer") {
+        card +=`
+        <a href="${element.gitHub}" class="card-link">Github</a>
+        `
+    }
+    if (`${element.role}` === "Intern") {
+        card += `
+        <p>Institution: ${element.school}</p>
+        `
+    }
+    card +=`
+    </div>
+    </div>
+    </div>
+    `
+
+    siteArray.push(card)
+
+})
+const htmlFoot = `
+</body>
+</html> 
+`
+siteArray.push(htmlFoot)
+
+fs.writeFile(`./dist/new_site.html`, siteArray.join(""), (err) => {
+    console.log(err)
+})
+
+}
+
+createManager()
