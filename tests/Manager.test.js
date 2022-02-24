d7f63a19da298c990405693dad9501c1f65b15a1
nathanrusselldev <nathanrussell.it@gmail.com>
2022-02-24 14:56:19 -0500

    updating test suite to function with my code

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,19 +2,25 @@ const Manager = require("../lib/manager");
 const Employee = require("../lib/employee");
 
 test("Can set office number via constructor argument", () => {
+
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
+
 });
 
-test("getRole() should return \"Manager\"", () => {
+test("can get role via args", () => {
+
   const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  const e = new Manager("Foo", 1, "test@test.com", testValue, );
   expect(e.getRole()).toBe(testValue);
+
 });
 
-test("Can get office number via getOffice()", () => {
-  const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
-  expect(e.getOfficeNumber()).toBe(testValue);
+test("Can get office number via args", () => {
+
+  const officeNumber = 100
+  const e = new Manager("Foo", 1, "test@test.com", officeNumber);
+  expect(e.getOfficeNumber()).toBe(officeNumber);
+
 });
